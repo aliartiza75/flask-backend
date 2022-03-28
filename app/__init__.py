41b164f3063f42a213fdf4dc9c591e76988a350c
aliartiza75 <aliartiza75@yahoo.com>
2022-03-28 23:12:23 +0200

    [add-sonar-checks] fix issues

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from app.datetime_api.routes import mod
 from flask import Flask
 
 app = Flask(__name__)
+app.config['WTF_CSRF_ENABLED'] = True
 
 # registering application
 app.register_blueprint(mod, uri_prefix='/datetime_api')
